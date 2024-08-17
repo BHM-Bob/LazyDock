5ce6581cb5c602b75fe0b8a7a250fe8f0c94f8e1
BHM-Bob <2262029386@qq.com>
2024-08-17 10:11:41 +0800

    fix-code: lazydock_pymol_plugin\__init__.py: add MBAPY_FAST_LOAD FLAG

--- lazydock_pymol_plugin/__init__.py
+++ lazydock_pymol_plugin/__init__.py
@@ -1,10 +1,18 @@
 '''
 Date: 2024-08-16 09:36:38
 LastEditors: BHM-Bob 2262029386@qq.com
-LastEditTime: 2024-08-16 11:05:48
-Description: 
+LastEditTime: 2024-08-17 09:41:41
+Description: LazyDock Pymol Plugin
 '''
 
+import os
+
+os.environ['MBAPY_FAST_LOAD'] = 'True'
+os.environ['MBAPY_AUTO_IMPORT_TORCH'] = 'False'
+
+
+from . import _autodock_utils
+from .lazy_dlg import LazyDLG
 from .lazy_pocket import LazyPocket
 
 
@@ -12,3 +20,5 @@ def __init__(self):
 	self.menuBar.addcascademenu('Plugin', 'LazyDockPymolPlugin', 'LazyDockPymolPlugin', label = 'LazyDockPymolPlugin')
 	self.menuBar.addmenuitem('LazyDockPymolPlugin', 'command', 'LazyPocket',
                           label = 'LazyPocket', command = lambda s=self : LazyPocket(s))
+	self.menuBar.addmenuitem('LazyDockPymolPlugin', 'command', 'LazyDLG',
+                          label = 'LazyDLG', command = lambda s=self : LazyDLG(s))
