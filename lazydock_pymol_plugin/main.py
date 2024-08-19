cdbd756ff130de7c134126f3749ff8f321222e65
BHM-Bob <2262029386@qq.com>
2024-08-19 22:08:20 +0800

    fix-bug: lazydock_pymol_plugin\main.py: GUILauncher: launch as sub-thread minor-addon: lazydock_pymol_plugin\main.py: GUILauncher: add ui_update_content_from_pymol

--- lazydock_pymol_plugin/main.py
+++ lazydock_pymol_plugin/main.py
@@ -1,29 +1,49 @@
 '''
 Date: 2024-08-19 10:41:56
 LastEditors: BHM-Bob 2262029386@qq.com
-LastEditTime: 2024-08-19 11:07:47
+LastEditTime: 2024-08-19 20:30:15
 Description: 
 '''
-from nicegui import app, ui
+from threading import Thread
 
-from .lazy_dlg import LazyDLG
-from .lazy_pocket import LazyPocket
+from lazy_dlg import LazyDLG
+from lazy_pocket import LazyPocket
+# from mbapy.web import TaskPool
+from nicegui import app, ui
+from pymol import cmd
 
 
 class GUILauncher:
     def __init__(self, app = None):
+        
+        self._now_molecule = cmd.get_names_of_type('object:molecule') or []
+        self._now_selection = cmd.get_names_of_type('selection') or []
+        self.ui_update_func = []
+        ui.timer(1, self.ui_update_content_from_pymol)
+        
         self.lazy_pocket = LazyPocket(self)
         self.lazy_dlg = LazyDLG(self)
         
         self.build_gui()
-        ui.run(title='LazyDock', url = 'http://localhost', port=8090)
         
-    def run_gui(self, ):
+        self.host_thread = Thread(name = 'GUI-Host', target=ui.run,
+                                  kwargs = dict(title='LazyDock', host = 'localhost', port=8090, reload = False),
+                                  daemon=False)
+        self.host_thread.run()
+        
+    def ui_update_content_from_pymol(self, ):
+        self._now_molecule = cmd.get_names_of_type('object:molecule')
+        self._now_selection = cmd.get_names_of_type('selection')
+        
+        for fn in self.ui_update_func:
+            fn()
+        
+    def build_gui(self, ):
         with ui.header(elevated=True).style('background-color: #3874c8'):
             ui.label('LazyDock | Pymol Plugin').classes('text-h4')
             ui.space()
             ui.button('Exit', on_click=app.shutdown, icon='power')
-        with ui.splitter(value=10).classes('w-full h-56') as splitter:
+        with ui.splitter(value=10).classes('w-full h-full') as splitter:
             with splitter.before:
                 with ui.tabs().props('vertical').classes('w-full') as tabs:
                     lazy_pocket_tab = ui.tab('Pocket')
@@ -34,10 +54,9 @@ class GUILauncher:
                     with ui.tab_panel(lazy_pocket_tab):
                         self.lazy_pocket.build_gui()
                     with ui.tab_panel(lazy_dlg_tab):
-                        self.lazy_pocket.build_gui()
-                
-    
+                        self.lazy_dlg.build_gui()
 
+    
 if __name__ in {"__main__", "__mp_main__"}:
     # dev code
-    GUILauncher()
+    app = GUILauncher()
