b29e8a3857bbeac9815b927d34e5d9e4486a2b47
BHM-Bob <2262029386@qq.com>
2024-08-24 21:34:21 +0800

    addon: lazydock\pml\autodock_utils.py: add DlgFile fix-code: lazydock\pml\autodock_utils.py: ADModel: optimize performance

--- lazydock/pml/autodock_utils.py
+++ lazydock/pml/autodock_utils.py
@@ -6,65 +6,118 @@ from __future__ import absolute_import, print_function
 import re
 import tkinter as tk
 import tkinter.filedialog as tkFileDialog
+from typing import Any, Callable, List
+from mbapy.file import opts_file, decode_bits_to_str
+from mbapy.game import BaseInfo
 
 # atom-type, atom-number, atom-name, residue-name, chain-name, residue-number, x, y, z, occupancy, temperature-factor
 # ATOM      1  CA  LYS     7     136.747 133.408 135.880 -0.06 +0.10
 PDB_PATTERN = r"(ATOM|HETATM) +(\d+) +(\w+) +(\w+) +(\w+)? +(\d+) +([\d\-\.]+) +([\d\-\.]+) +([\d\-\.]+) +([\+\-][\d\-\.]+) +([\+\-][\d\-\.]+)"
 
 
-class ADModel:
-    """ STORAGE CLASS FOR DOCKED LIGANDS """
-    def __init__(self, lst=None, sort_atom_by_res: bool = False):
-        self.atomlines = []
+
+class ADModel(BaseInfo):
+    """STORAGE CLASS FOR DOCKED LIGAND"""
+    def __init__(self, content: str = None, _sort_atom_by_res: bool = False):
         self.energy = 0.
         self.name = ''
         self.poseN = 0
         self.info = ''
-        self.lst = []
-        self.num = 0
-        self.as_string = ''
+        self.pdb_string = ''
         self.pdb_lines = []
-        if lst is not None:
-            self.from_list(lst, sort_atom_by_res)
+        if content is not None:
+            self.decode_content(content, _sort_atom_by_res)
             
-    def sort_atom_by_res(self, pdb_string: str = None):
-        pdb_string = pdb_string or self.as_string
-        pdb_string_lines = pdb_string.strip().split('\n')
-        for idx, line in enumerate(pdb_string_lines):
-            matches = re.findall(PDB_PATTERN, line)
-            items = list(matches[0])
-            items[1], items[5] = int(items[1]), int(items[5])
-            self.pdb_lines.append(items+[idx]) # line items + line idx
-        # sort by chain name first, then by residue number, then by atom number
-        self.pdb_lines.sort(key=lambda x: (x[4], x[5], x[1]))
-        # apply sorted line idx to as_string
-        self.as_string = '\n'.join([pdb_string_lines[i[-1]] for i in self.pdb_lines])
-        return self.as_string
-            
-    def from_list(self, lst, sort_atom_by_res: bool = False):
-        self.info = '\n'.join(lst)
-        self.lst = lst
-        for line in lst:
-            if line.startswith('ATOM') or \
-                    line.startswith('HETATM'):
-                self.atomlines.append(line)
-                self.as_string += 'ATOM  ' + line[6:67] + '\n'
-            elif line.startswith('USER'):
-                if 'Free Energy of Binding' in line:
-                    entr = line.split('=')[1]
-                    self.energy = float(entr.split()[0])
-            elif line.startswith('REMARK'):
-                if 'VINA RESULT' in line:
-                    entr = line.split(':')[1]
-                    self.energy = float(entr.split()[0])
-        if sort_atom_by_res:
-            self.sort_atom_by_res()
+    def decode_content(self, content: str, _sort_atom_by_res: bool = False):
+        # parse pdb lines
+        self.info = content
+        self.pdb_lines = list(map(lambda x: x[0], re.findall(r'((ATOM|HETATM).+?\n)', self.info)))
+        self.pdb_atoms = re.findall(PDB_PATTERN, self.info)
+        if _sort_atom_by_res:
+            pack = sorted(zip(self.pdb_lines, self.pdb_atoms), key = lambda x : (x[1][4], int(x[1][5]), int(x[1][1])))
+            self.pdb_lines, self.pdb_atoms = zip(*pack)
+        self.pdb_string = ''.join(self.pdb_lines)
+        # parse energy could be str.find?
+        energy_line = re.findall(r'USER.+?Free Energy of Binding.+?\n', self.info)
+        if energy_line:
+            entr = energy_line[0].split('=')[1]
+            self.energy = float(entr.split()[0])
+        else:
+            energy_line = re.findall(r'REMARK.+?VINA RESULT.+?\n', self.info)
+            if energy_line:
+                entr = energy_line[0].split(':')[1]
+                self.energy = float(entr.split()[0])
+            else:
+                self.energy = None
 
     def as_pdb_string(self):
-        return self.as_string
+        return self.pdb_string
         
     def info_string(self):
         return self.info
+    
+
+class DlgFile(BaseInfo):
+    def __init__(self, path: str = None, content: str = None,
+                 sort_pdb_line_by_res: bool = False):
+        # load content from file if path is provided
+        self.path = path
+        self.sort_pdb_line_by_res = sort_pdb_line_by_res
+        if path is not None:
+            self.content = decode_bits_to_str(opts_file(path, 'rb'))
+        elif content is not None:
+            self.content = content
+        # decode content to pose_lst
+        if self.content is not None:
+            self.pose_lst: List[ADModel] = self.decode_content()
+            self.sort_pose()
+        else:
+            self.pose_lst: List[ADModel] = []
+        self.n2i = {}
+        
+    def sort_pose(self, key: Callable[[ADModel], Any] = None,
+                  inplace: bool = True, reverse: bool = False) -> None:
+        if key is None:
+            key = lambda x : x.energy
+        if inplace:
+            self.pose_lst.sort(key=key, reverse=reverse)
+            ret = self.pose_lst
+        else:
+            ret = sorted(self.pose_lst, key=key, reverse=reverse)
+        return ret
+        
+    def decode_content(self):
+        dlg_pose_lst = []
+        for model in re.findall('MODEL.+?ENDMDL', self.content, re.DOTALL):
+            model = model.replace('\nDOCKED: ', '\n')
+            dlg_pose_lst.append(ADModel(model, self.sort_pdb_line_by_res))
+        return dlg_pose_lst
+    
+    def asign_pose_name(self, pose_names: List[str]):
+        if len(pose_names) != len(self.pose_lst):
+            raise ValueError("Number of pose names must match number of poses")
+        for i, name in enumerate(pose_names):
+            self.n2i[name] = i
+        
+    def asign_kw_prop(self, key: str, value: List[Any]):
+        if value is None:
+            if len(value) == len(self.pose_lst):
+                setattr(self, key, value)
+            
+    def get_pose(self, pose_name: str = None, pose_idx: int = None):
+        if pose_name is None and pose_idx is None:
+            raise ValueError("Either pose_name or pose_idx must be provided")
+        if pose_name is not None:
+            pose_idx = self.n2i[pose_name]
+        return self.pose_lst[pose_idx]
+    
+    def get_pose_prop(self, prop: str, pose_name: str = None, pose_idx: int = None):
+        if pose_name is None and pose_idx is None:
+            raise ValueError("Either pose_name or pose_idx must be provided")
+        if pose_name is not None:
+            pose_idx = self.n2i[pose_name]
+        return getattr(self, prop)[pose_idx]
+
 
 
 def tk_file_dialog_wrapper(*args, **kwargs):
@@ -98,3 +151,12 @@ class MyFileDialog:
     @tk_file_dialog_wrapper()
     def get_ask_dir(self, parent):
         return tkFileDialog.askdirectory(parent=parent, initialdir = self.initialdir)
+
+
+if __name__ == '__main__':
+    # from mbapy.base import TimeCosts
+    # @TimeCosts(10)
+    # def load_test(idx):
+    #     DlgFile(path='data_tmp/dlg/1000run.dlg', sort_pdb_line_by_res=True)
+    # load_test()
+    DlgFile(path='data_tmp/dlg/1000run.dlg', sort_pdb_line_by_res=True)
