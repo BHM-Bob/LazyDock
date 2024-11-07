1c93503291068da9c3cee5c963bb9acbf15f7427
BHM-Bob <2262029386@qq.com>
2024-11-07 22:35:51 +0800

    minor-addon: lazydock\config.py: Config: add load and dump method

--- lazydock/config.py
+++ lazydock/config.py
@@ -1,7 +1,7 @@
 '''
 Date: 2024-11-05 16:27:41
 LastEditors: BHM-Bob 2262029386@qq.com
-LastEditTime: 2024-11-05 16:38:24
+LastEditTime: 2024-11-07 22:35:10
 Description: 
 '''
 import os
@@ -9,6 +9,7 @@ from pathlib import Path
 
 from mbapy_lite.game import BaseInfo
 
+CONFIG_FILE_PATH = Path('~/.lazydock/lazydock_config.json').expanduser()
 
 class Config(BaseInfo):
     def __init__(self, ):
@@ -17,8 +18,14 @@ class Config(BaseInfo):
             'ligplus_dir': None,
         }
         
+    def load(self, path: str = CONFIG_FILE_PATH):
+        self.from_json(path)
+        
+    def dump(self, path: str = CONFIG_FILE_PATH):
+        self.to_json(path)
+        
+
 configs = Config()
-CONFIG_FILE_PATH = Path('~/.lazydock/lazydock_config.json').expanduser()
 
 if not os.path.exists(CONFIG_FILE_PATH.parent):
     os.makedirs(CONFIG_FILE_PATH.parent)
