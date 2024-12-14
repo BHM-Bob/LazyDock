a13d00a4133f1180ab7637e315da7e7353bbc3f5
BHM-Bob <2262029386@qq.com>
2024-12-14 21:51:33 +0800

    minor-addon: lazydock_pymol_plugin\__init__.py: add align_pose_to_axis command to LazyDock Pymol Plugin
    - Import align_pose_to_axis function from lazydock.pml.align_to_axis
    - Extend pymol command with align_pose_to_axis function
    - Update last edit time to 2024-12-14 21:45:28

--- lazydock_pymol_plugin/__init__.py
+++ lazydock_pymol_plugin/__init__.py
@@ -1,7 +1,7 @@
 '''
 Date: 2024-08-16 09:36:38
 LastEditors: BHM-Bob 2262029386@qq.com
-LastEditTime: 2024-09-03 16:14:07
+LastEditTime: 2024-12-14 21:45:28
 Description: LazyDock Pymol Plugin
 '''
 
@@ -17,6 +17,7 @@ from main import GUILauncher
 from pymol import cmd
 
 from lazydock.pml.server import VServer
+from lazydock.pml.align_to_axis import align_pose_to_axis
 
 
 def __init__(self):
@@ -37,3 +38,4 @@ def start_lazydock_server(host: str = 'localhost', port: int = 8085, quiet: int 
     
     
 cmd.extend('start_lazydock_server', start_lazydock_server)
+cmd.extend('align_pose_to_axis', align_pose_to_axis)
