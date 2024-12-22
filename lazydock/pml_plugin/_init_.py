6f28914314dd30dfe34db5cb7923216b1c27ae77
BHM-Bob <2262029386@qq.com>
2024-12-22 20:47:58 +0800

    addon: lazydock\pml_plugin\_init_.py: add open_vina_config_as_box: add function to visualize Vina config as box and improve plugin loading
    - Add open_vina_config_as_box function to draw a box based on Vina config file
    - Print available commands when plugin is loaded for better user guidance
    - Update LastEditTime to 2024-12-22 20:46:09

--- lazydock/pml_plugin/_init_.py
+++ lazydock/pml_plugin/_init_.py
@@ -1,10 +1,10 @@
 '''
 Date: 2024-12-15 19:25:42
 LastEditors: BHM-Bob 2262029386@qq.com
-LastEditTime: 2024-12-20 15:32:47
+LastEditTime: 2024-12-22 20:46:09
 Description: 
 '''
-
+import os
 from typing import List, Union
 
 from pymol import cmd
@@ -24,3 +24,30 @@ def align_pose_to_axis_warp(pml_name: str, move_name: str = None, fixed: Union[L
     align_pose_to_axis(pml_name, move_name, fixed, state, move_method, dss, quiet)
 
 cmd.extend('align_pose_to_axis', align_pose_to_axis_warp)
+
+
+def open_vina_config_as_box(config_path: str, spacing: float = 1.0):
+    from mbapy_lite.file import opts_file
+
+    from lazydock.pml.thirdparty.draw_bounding_box import draw_box
+    if not os.path.exists(config_path):
+        return print(f'Config file {config_path} not found, skip.')
+    cfg = opts_file(config_path, way='lines')
+    get_line = lambda n: [line for line in cfg if line.startswith(n)][0]
+    center = {line.split('=')[0].strip(): float(line.split('=')[1].strip()) for line in map(get_line, ['center_x', 'center_y', 'center_z'])}
+    size = {line.split('=')[0].strip(): float(line.split('=')[1].strip()) for line in map(get_line, ['size_x', 'size_y', 'size_z'])}
+    print(f'center: {center}, size: {size}')
+    minz, miny, minz = [(center[f'center_{k}'] - size[f'size_{k}'] / 2) * spacing for k in ['x', 'y', 'z']]
+    maxz, maxy, maxz = [(center[f'center_{k}'] + size[f'size_{k}'] / 2) * spacing for k in ['x', 'y', 'z']]
+    draw_box(minz, miny, minz, maxz, maxy, maxz)
+    
+cmd.extend('open_vina_config_as_box', open_vina_config_as_box)
+
+
+print('LazyDock plugin loaded.')
+print('''
+Commands (python API):
+    start_lazydock_server(host='localhost', port=8085, quiet=1)
+    align_pose_to_axis(pml_name, move_name='', fixed='center', state=0, move_method='rotate', dss=1, quite=0)
+    open_vina_config_as_box(config_path, spacing=1.0)
+''')
