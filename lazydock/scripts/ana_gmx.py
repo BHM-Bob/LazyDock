a4d8b393749c9dd7f8821e81c5a790ac922450cd
BHM-Bob <2262029386@qq.com>
2025-01-14 21:36:45 +0800

    fix-bug: lazydock\scripts\ana_gmx.py: simple: add -c 1 option to dit xvg_compare for consistency
    - Added -c 1 option to dit xvg_compare commands in rms, rmsf, and gyrate functions
    - Updated sasa function to use '>' instead of 'Select a group:' for group selection
    - Removed redundant empty lines and improved code formatting

--- lazydock/scripts/ana_gmx.py
+++ lazydock/scripts/ana_gmx.py
@@ -46,19 +46,19 @@ class simple(Command):
     def rms(gmx: Gromacs, main_name: str, group: str = '4', **kwargs):
         gmx.run_command_with_expect('rms', s=f'{main_name}.tpr', f=f'{main_name}_center.xtc', o=f'rmsd.xvg', tu='ns',
                                     expect_actions=[{'Select a group:': f'{group}\r'}, {'Select a group:': f'{group}\r'}], **kwargs)
-        os.system(f'cd "{gmx.working_dir}" && dit xvg_compare -f rmsd.xvg -o rmsd.png -smv -t "RMSD of {main_name}" -csv {main_name}_rmsd.csv')
+        os.system(f'cd "{gmx.working_dir}" && dit xvg_compare -c 1 -f rmsd.xvg -o rmsd.png -smv -t "RMSD of {main_name}" -csv {main_name}_rmsd.csv')
         
     @staticmethod
     def rmsf(gmx: Gromacs, main_name: str, group: str = '4', res: bool = True, **kwargs):
         gmx.run_command_with_expect('rmsf', s=f'{main_name}.tpr', f=f'{main_name}_center.xtc', o=f'rmsf.xvg', res=res,
                                     expect_actions=[{'Select a group:': f'{group}\r'}], **kwargs)
-        os.system(f'cd "{gmx.working_dir}" && dit xvg_compare -f rmsf.xvg -o rmsf.png -smv -t "RMSF of {main_name}" -csv {main_name}_rmsf.csv')
+        os.system(f'cd "{gmx.working_dir}" && dit xvg_compare -c 1 -f rmsf.xvg -o rmsf.png -smv -t "RMSF of {main_name}" -csv {main_name}_rmsf.csv')
         
     @staticmethod
     def gyrate(gmx: Gromacs, main_name: str, group: str = '4', **kwargs):
         gmx.run_command_with_expect('gyrate', s=f'{main_name}.tpr', f=f'{main_name}_center.xtc', o=f'gyrate.xvg',
                                     expect_actions=[{'Select a group:': f'{group}\r'}], **kwargs)
-        os.system(f'cd "{gmx.working_dir}" && dit xvg_compare -f gyrate.xvg -o gyrate.png -smv -t "Gyrate of {main_name}" -csv {main_name}_gyrate.csv')
+        os.system(f'cd "{gmx.working_dir}" && dit xvg_compare -c 1 -f gyrate.xvg -o gyrate.png -smv -t "Gyrate of {main_name}" -csv {main_name}_gyrate.csv')
         
     @staticmethod
     def hbond(gmx: Gromacs, main_name: str, group: str = '1', dt=10, **kwargs):
@@ -67,15 +67,15 @@ class simple(Command):
                                     expect_actions=[{'Select a group:': f'{group}\r'}, {'Select a group:': f'{group}\r'}], **kwargs)
         os.system(f'cd "{gmx.working_dir}" && dit xvg_compare -c 1 -f {main_name}_hbond_num.xvg -o hbond_num.png -smv -t "H-bond num of {main_name}" -csv {main_name}_hbond_num.csv')
         os.system(f'cd "{gmx.working_dir}" && dit xvg_show -f {main_name}_hbond_dist.xvg -o hbond_dist.png')
-        
+
     @staticmethod
     def sasa(gmx: Gromacs, main_name: str, group: str = '4', **kwargs):
         gmx.run_command_with_expect('sasa -or sasa_res.xvg', s=f'{main_name}.tpr', f=f'{main_name}_center.xtc',
                                     o=f'sasa_total.xvg', odg=f'sasa_dg.xvg', tv='sasa_tv.xvg', tu='ns',
-                                    expect_actions=[{'Select a group:': f'{group}\r'}], **kwargs)
+                                    expect_actions=[{'>': f'{group}\r'}], **kwargs)
         for ty in ['total', 'res', 'dg', 'tv']:
             os.system(f'cd "{gmx.working_dir}" && dit xvg_compare -c 1 -f sasa_{ty}.xvg -o sasa_{ty}.png -smv -t "SASA {ty} of {main_name}" -csv {main_name}_sasa_{ty}.csv')
-        
+
     @staticmethod
     def covar(gmx: Gromacs, main_name: str, xmax: str = 15, **kwargs):
         gmx.run_command_with_expect('covar', s=f'{main_name}.tpr', f=f'{main_name}_center.xtc', o=f'eigenval.xvg', tu='ns',
