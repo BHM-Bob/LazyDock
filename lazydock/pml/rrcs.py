18491d7cad31917d94eff2a646c6b2a3732d7d77
BHM-Bob <2262029386@qq.com>
2025-02-14 13:24:29 +0800

    addon: lazydock/pml/rrcs.py: add calcu_RRCS_from_array
    - Implement a new NumPy-based backend for RRCS calculation
    - Improve performance by using vectorized operations and reducing memory usage
    - Add support for calculating RRCS from NumPy arrays
    - Retain compatibility with the existing dictionary-based backend

--- lazydock/pml/rrcs.py
+++ lazydock/pml/rrcs.py
@@ -1,11 +1,12 @@
 '''
 Date: 2024-09-30 19:28:57
 LastEditors: BHM-Bob 2262029386@qq.com
-LastEditTime: 2025-02-13 21:33:30
+LastEditTime: 2025-02-14 13:23:01
 Description: RRCS calculation in PyMOL, RRCS is from article "Common activation mechanism of class A GPCRs": https://github.com/elifesciences-publications/RRCS/blob/master/RRCS.py
 '''
 from typing import Dict, Tuple
 
+import numpy as np
 import pandas as pd
 from pymol import cmd
 
@@ -81,7 +82,46 @@ def calcu_RRCS_from_dict(dict_coord: Dict[str, Dict[int, Tuple[float, float, flo
     return contact_df
 
 
-def calcu_RRCS(model: str, _cmd = None):
+def calcu_RRCS_from_array(names: np.ndarray, resis: np.ndarray, positions: np.ndarray, occupancies: np.ndarray):
+    """
+    Parameters:
+        - names: [N, ], np.ndarray of atom names
+        - resns: [N, ], np.ndarray of residue names
+        - resis: [N, ], np.ndarray of residue numbers
+        - positions: [N, 3], np.ndarray of atom positions, shape (n_atoms, 3)
+        - occupancies: [N, ], np.ndarray of atom occupancies
+        
+    Returns:
+        contact_df: DataFrame of contact scores, index and columns are residue names.
+    """
+    # calcu mask
+    check_atm_mask = np.abs(resis[:, None] - resis[None, :]) < 5
+    atm_mask = np.in1d(names, np.array(['N', 'CA', 'C', 'O']))
+    atm_mask = atm_mask[:, None] | atm_mask[None, :]
+    ij_less_mask = (resis[:, None] > resis[None, :]).astype(np.int32)
+    # calcu matrix
+    d1_mat = np.abs(positions[:, None, :] - positions[None, :, :])# [N, N, 3]
+    d2_mat = ((positions[:, None, :] - positions[None, :, :])**2).sum(axis=-1) # [N, N]
+    close_mask = (d1_mat < 4.63).all(axis=-1, keepdims=False).astype(np.int32) # [N, N]
+    ooc_mat = occupancies[:, None] * occupancies[None, :]
+    # calculate RRCS score for each atom pair
+    score_mat = np.where(d2_mat < 10.4329, ooc_mat, 0.0)
+    score_mat = np.where((d2_mat >= 10.4329) & (d2_mat < 21.4369), (1-(d2_mat**0.5 - 3.23)/1.4) * ooc_mat, score_mat)
+    score_mat = np.where(check_atm_mask & atm_mask, 0, score_mat * close_mask * ij_less_mask)
+    # 获取分组的唯一值及其索引
+    unique_values, row_labels = np.unique(resis, return_inverse=True)
+    K = len(unique_values)
+    idx_mat = (row_labels[:, None] * K + row_labels[None, :]).reshape(-1)
+    matrix_flat = score_mat.ravel()
+    result_flat = np.bincount(idx_mat, weights=matrix_flat, minlength=K**2)
+    # 重新排布为 K x K 矩阵
+    result = result_flat.reshape(K, K)
+    contact_df = pd.DataFrame(result, index=unique_values, columns=unique_values)
+    contact_df.to_excel('RRCS.xlsx')
+    return contact_df
+
+
+def calcu_RRCS(model: str, backend: str = 'numpy', _cmd = None):
     """
     Parameters:
         - model: molecular name loaded in pymol
@@ -91,18 +131,38 @@ def calcu_RRCS(model: str, _cmd = None):
         contact_df: DataFrame of contact scores, index and columns are residue names.
     """
     _cmd = _cmd or cmd
-    dict_coord = {} # dict to store coordinates. dict_coord[res][atom] = (x, y, z, occupancy)
-    _cmd.iterate_state(1, model, 'dict_coord.setdefault(f"{chain}:{resi}:{resn}", {}).setdefault(index, (x, y, z, q))', space={'dict_coord': dict_coord})
-    atomnum2name = {} # map atom number to atom name, in order to find N, CA, C, O
-    _cmd.iterate(model, 'atomnum2name[index] = name', space={'atomnum2name': atomnum2name})
-    return calcu_RRCS_from_dict(dict_coord, atomnum2name)
+    if backend == 'numpy':
+        # Prepare PyMOL data
+        resis, names, positions, occupancies = [], [], [], []
+        _cmd.iterate_state(1, model, 'resis.append(resi); names.append(name); '
+                                'positions.append((x, y, z)); occupancies.append(q)',
+                        space={'resis': resis, 'names': names, 'positions': positions, 
+                                'occupancies': occupancies})
+        # Convert to NumPy arrays
+        resis = np.array(resis, dtype=int)
+        sort_idx = np.argsort(resis)
+        resis = resis[sort_idx]
+        names = np.array(names)[sort_idx]
+        positions = np.array(positions)[sort_idx]
+        occupancies = np.array(occupancies)[sort_idx]
+        return calcu_RRCS_from_array(names, resis, positions, occupancies)
+    elif backend == 'dict':
+        dict_coord = {} # dict to store coordinates. dict_coord[res][atom] = (x, y, z, occupancy)
+        _cmd.iterate_state(1, model, 'dict_coord.setdefault(f"{chain}:{resi}:{resn}", {}).setdefault(index, (x, y, z, q))', space={'dict_coord': dict_coord})
+        atomnum2name = {} # map atom number to atom name, in order to find N, CA, C, O
+        _cmd.iterate(model, 'atomnum2name[index] = name', space={'atomnum2name': atomnum2name})
+        return calcu_RRCS_from_dict(dict_coord, atomnum2name)
+    else:
+        raise ValueError('backend must be numpy or dict')
 
 
 if __name__ == '__main__':
+    import time
     cmd.reinitialize()
     cmd.load('data_tmp/pdb/RECEPTOR.pdb', 'receptor')
-    from mbapy_lite.base import TimeCosts
-    @TimeCosts(3)
-    def test_calcu(idx):
-        calcu_RRCS('receptor')
-    test_calcu()
+    calcu_RRCS('receptor', backend='numpy')
+    # from mbapy_lite.base import TimeCosts
+    # @TimeCosts(3)
+    # def test_calcu(idx):
+    #     calcu_RRCS('receptor', backend='numpy')
+    # test_calcu()
