b95d77cca3555b5438ec4b1d903a038f11e7de9a
BHM-Bob <2262029386@qq.com>
2025-02-19 22:36:24 +0800

    fix-bug: lazydock/scripts/ana_gmx.py: interaction: close plot to prevent multi cbar
    - Add plt.close() to close the plot and free up memory
    - Save interaction data as a CSV file for further analysis

--- lazydock/scripts/ana_gmx.py
+++ lazydock/scripts/ana_gmx.py
@@ -431,8 +431,10 @@ class interaction(simple_analysis, mmpbsa):
                         else:
                             plot_df.loc[i, receptor_res] += 1
             plot_df /= self.args.plot_time_unit
+            plot_df.to_csv(str(top_path.parent / f'{top_path.stem}_{self.args.method}_plot_df.csv'), index=False)
             sns.heatmap(plot_df, xticklabels=list(plot_df.columns))
             save_show(str(top_path.parent / f'{top_path.stem}_{self.args.method}_interactions.png'), 600, show=False)
+            plt.close()
             # other things
             pool.task = {}
             bar.update(1)
