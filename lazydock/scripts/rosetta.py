3ef5608a7727025e37b182a2781f507f0f46341e
BHM-Bob <2262029386@qq.com>
2025-12-30 20:36:35 +0800

    feat(lazydock/scripts/rosetta.py): Add structural relaxation functionality and support parallel processing Realize the relaxation function of the PDB structure, including the comparison before and after energy calculation Support multi-process parallel processing to improve processing efficiency

--- lazydock/scripts/rosetta.py
+++ lazydock/scripts/rosetta.py
@@ -9,14 +9,16 @@ import os
 from pathlib import Path
 from typing import List, Optional
 
-from tqdm import tqdm
 import pandas as pd
 import pyrosetta
 from mbapy_lite.base import put_err
 from mbapy_lite.file import get_paths_with_extension
 from mbapy_lite.web import TaskPool
+from tqdm import tqdm
 
-from lazydock.pyrt.energy_utils import calcu_interface_energy
+from lazydock.pyrt.energy_utils import calcu_interface_energy, calcu_single_energy
+from lazydock.pyrt.pose_utils import load_pose
+from lazydock.pyrt.relax import relax_pdb
 from lazydock.scripts._script_utils_ import Command, excute_command
 
 
@@ -69,10 +71,63 @@ class cacl_energy(Command):
         df.to_csv(self.args.output, index=False)
 
 
+def _relax_worker(pdb_path: str, output_path: str, chain: str, max_iter: int):
+    pose = load_pose(pdb_path)
+    energy_0 = calcu_single_energy(pose)
+    pose = relax_pdb(pose, output_path, chain, max_iter)
+    energy_1 = calcu_single_energy(pose)
+    return pdb_path, output_path, energy_0, energy_1
+
+
+class relax(cacl_energy):
+    def __init__(self, args, printf=print):
+        super().__init__(args, printf)
+    
+    @staticmethod
+    def make_args(args: argparse.ArgumentParser):
+        args.add_argument('-d', '--batch-dir', nargs='+', type=str, default=['.'],
+                          help='Batch directory path. default: current directory.')
+        args.add_argument('-n', '--name', type=str, default='',
+                          help='pdb file name substring. default: empty.')
+        args.add_argument('-c', '--relax-chain', type=str, nargs='+', required=True,
+                          help='Chain ID to relax. required.')
+        args.add_argument('-it', '--max-iter', type=int, default=3,
+                          help='Maximum iterations for relaxation. default: 3.')
+        args.add_argument('-o', '--output_suffix', type=str, default='_relaxed',
+                          help='Output PDB file suffix. default: _relaxed.')
+        args.add_argument('-s', '--summary', type=str, default='relax_summary.csv',
+                          help='Summary CSV file path. default: relax_summary.csv')
+        args.add_argument('-nw', '--n-workers', type=int, default=1,
+                          help='Number of workers. default: 1.')
+        return args
+    
+    def main_process(self):
+        pdb_paths = get_paths_with_extension(self.args.batch_dir, ['.pdb'], name_substr=self.args.name)
+        df = pd.DataFrame(columns=['pdb_path', 'relaxed_pdb_path', 'energy_before', 'energy_after'])
+        # parallel
+        if self.args.n_workers > 1:
+            pool = TaskPool('process', self.args.n_workers, report_error=True).start()
+        # Process each PDB file
+        for pdb_path in tqdm(pdb_paths, desc='Relaxing structures'):
+            output_path = pdb_path.replace('.pdb', f'{self.args.output_suffix}.pdb')
+            if self.args.n_workers > 1:
+                pool.add_task(pdb_path, _relax_worker, pdb_path, output_path,
+                                                     self.args.relax_chain, self.args.max_iter)
+                pool.wait_till_free()
+            else:
+                df.loc[len(df)] = list(_relax_worker(pdb_path, output_path,
+                                                     self.args.relax_chain, self.args.max_iter))
+        if self.args.n_workers > 1:
+            for pdb_path in tqdm(list(pool.tasks.keys()), desc='Querying results from TaskPool'):
+                df.loc[len(df)] = list(pool.query_task(pdb_path, block=True, timeout=30))  # pyright: ignore[reportArgumentType]
+            pool.close(1)
+        # Save summary
+        df.to_csv(self.args.summary, index=False)
+
 
 _str2func = {
     'calc-energy': cacl_energy,
-    # 'relax': relax,
+    'relax': relax,
 }
 
 
