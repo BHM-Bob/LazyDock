1386263ae094f6f83e32096b35a23a5a2bc27b18
BHM-Bob <2262029386@qq.com>
2026-04-06 15:57:50 +0800

    refactor(lazydock/scripts/distribute_file.py): 简化参数命名并增加默认值
    - 将参数 `--source-root` 重命名为更简洁的 `--source` 并支持单文件路径
    - 为 `--dist-name` 参数添加默认空字符串值
    - 移除文件头部的注释信息
    - 优化文件搜索逻辑，支持直接指定单个文件

--- lazydock/scripts/distribute_file.py
+++ lazydock/scripts/distribute_file.py
@@ -1,10 +1,3 @@
-'''
-Date: 2024-12-13 20:18:59
-LastEditors: BHM-Bob 2262029386@qq.com
-LastEditTime: 2025-05-17 12:23:43
-Description: steps most from http://www.mdtutorials.com/gmx
-'''
-
 import argparse
 import os
 import shutil
@@ -19,8 +12,8 @@ from lazydock.scripts._script_utils_ import clean_path, show_args
 
 def main(sys_args: List[str] = None):
     args_paser = argparse.ArgumentParser(description = 'Get pocket residues from ProteinPlus web server.')
-    args_paser.add_argument('-sr', '--source-root', type = str,
-                            help='root to the files or directory to be distributed.')
+    args_paser.add_argument('-s', '--source', type = str,
+                            help='root to the files or directory OR a single file path to be distributed.')
     args_paser.add_argument('-sn', '--source-name', type = str,
                             help='sub string in source files or directory name.')
     args_paser.add_argument('-st', '--source-type', type = str, nargs='+', default=[],
@@ -29,7 +22,7 @@ def main(sys_args: List[str] = None):
                             help='source search mode, file or directory. Default is %(default)s.')
     args_paser.add_argument('-dr', '--dist-root', type = str,
                             help='path to the dist root directory.')
-    args_paser.add_argument('-dn', '--dist-name', type = str,
+    args_paser.add_argument('-dn', '--dist-name', type = str, default='',
                             help='sub string in dist directory name.')
     args_paser.add_argument('-dt', '--dist-type', type = str, nargs='+', default=[],
                             help='file type in dist directory. Default is %(default)s.')
@@ -41,18 +34,21 @@ def main(sys_args: List[str] = None):
                             help='sub string in file name. Default is %(default)s.')
     args = args_paser.parse_args(sys_args)
     # process IO path
-    args.source_root = clean_path(args.source_root)
+    args.source = clean_path(args.source)
     args.dist_root = clean_path(args.dist_root)
     # show args
-    show_args(args, ['source_root', 'source_name', 'source_type', 'mode', 'dist_root',
+    show_args(args, ['source', 'source_name', 'source_type', 'mode', 'dist_root',
                      'dist_name', 'dist_type', 'item_name', 'new_name'])
     # search source files or directories
     if args.mode == 'file':
-        sources = get_paths_with_extension(args.source_root, args.source_type, name_substr=args.source_name)
+        if os.path.isfile(args.source):
+            sources = [args.source]
+        else:
+            sources = get_paths_with_extension(args.source, args.source_type, name_substr=args.source_name)
     else:
-        sources = get_dir(args.source_root, 0, None, args.source_type, True, args.source_name)
+        sources = get_dir(args.source, 0, None, args.source_type, True, args.source_name)
     if not sources:
-        return put_err(f'No dist directory found in {args.source_root}, exit.')
+        return put_err(f'No dist directory found in {args.source}, exit.')
     # search dist directories
     dist_dirs = get_dir(args.dist_root, file_extensions=args.dist_type, recursive=True, dir_name_substr=args.dist_name)
     if not dist_dirs:
