9af3cb60a895d8d975f08067ff992ca8208307cb
BHM-Bob <2262029386@qq.com>
2026-09-10 11:57:04 +0800

    v0.16.0

--- lazydock/__version__.py
+++ lazydock/__version__.py
@@ -1,15 +1,15 @@
 '''
 Date: 2024-05-06 17:19:43
 LastEditors: BHM-Bob 2262029386@qq.com
-LastEditTime: 2025-05-28 16:09:39
+LastEditTime: 2026-09-10 11:56:10
 Description: 
 '''
 
 __title__ = "lazydock"
 __description__ = "Lazy Dock"
 __url__ = "https://github.com/BHM-Bob/LazyDock"
-__version__ = "0.15.0"
-__build__ = "20260510_0"
+__version__ = "0.16.0"
+__build__ = "20260910_0"
 __author__ = "BHM-Bob G"
 __author_email__ = "bhmfly@foxmail.com"
 __license__ = "MIT"
