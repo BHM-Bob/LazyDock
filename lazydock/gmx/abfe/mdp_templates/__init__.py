c5c3787288a5191d0f5e628a0e4bd4b14e7551b3
BHM-Bob <2262029386@qq.com>
2026-09-10 14:30:19 +0800

    add static files for ABFE

---
+++ lazydock/gmx/abfe/mdp_templates/__init__.py
@@ -0,0 +1,4 @@
+# mdp templates for ABFE.  Paths are resolved lazily via
+# lazydock.gmx.abfe.templates.TemplatePath (localized, no BindFlow dependency).
+
+from lazydock.gmx.abfe.templates import TemplatePath
